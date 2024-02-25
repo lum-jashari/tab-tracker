8f874bf3621ed500dc7453daf55cad8295084c98
lum-jashari <lumijashari@hotmail.com>
2024-02-25 23:28:09 +0100

    added create song UI

---
+++ client/src/components/CreateSongs.vue
@@ -0,0 +1,54 @@
+<template>
+    <div class="wrapper">
+        <div class="inputFieldsLeft">
+            <panel title="Song Metadata">
+                <v-text-field label="Title" v-model="title" />
+                <v-text-field label="Artist" v-model="artist" />
+                <v-text-field label="Genre" v-model="genre" />
+                <v-text-field label="Album" v-model="album" />
+                <v-text-field label="Album Image Url" v-model="albumImageUrl" />
+                <v-text-field label="YouTubeID" v-model="youtubeId" />
+            </panel>
+        </div>
+        <div class="inputFieldsRight">
+            <panel title="Song Structure">
+                <v-textarea label="Lyrics" counter v-model="lyrics" />
+                <v-textarea label="Tab" counter v-model="tab" />
+            </panel>
+        </div>
+    </div>
+</template>
+
+<script>
+import Panel from "@/components/Panel.vue";
+export default {
+    components: {
+        Panel,
+    },
+    data() {
+        return {
+            title: null,
+            artist: null,
+            genre: null,
+            album: null,
+            albumImageUrl: null,
+            youtubeId: null,
+            lyrics: null,
+            tab: null,
+        };
+    },
+};
+</script>
+
+<style scoped>
+.inputFieldsLeft {
+    width: 30%;
+}
+.inputFieldsRight {
+    width: 70%;
+    margin-left: 1em;
+}
+.wrapper {
+    display: flex;
+}
+</style>
--- client/src/router/index.js
+++ client/src/router/index.js
@@ -3,6 +3,7 @@ import Register from "@/components/Register.vue";
 import Login from "@/components/Login.vue";
 import Hello from "@/components/Hello.vue";
 import Songs from "@/components/Songs.vue";
+import CreateSongs from "@/components/CreateSongs.vue";
 
 const router = createRouter({
     history: createWebHistory(import.meta.env.BASE_URL),
@@ -26,6 +27,11 @@ const router = createRouter({
             path: "/songs",
             name: "songs",
             component: Songs,
+        },
+        {
+            path: "/songs/create",
+            name: "songs-create",
+            component: CreateSongs,
         },
     ],
 });
